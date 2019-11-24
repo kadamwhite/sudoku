e0bff4fd81147e8bba697c2767c9a0aa4e26454d
K Adam White <kadamwhite@users.noreply.github.com>
2019-11-24 12:25:20 -0500

    Add cell possibility previews

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const options = {
+  1: true,
+  2: true,
+  3: true,
+  4: true,
+  5: true,
+  6: true,
+  7: true,
+  8: true,
+  9: true,
+};
+
+function getRange( index ) {
+  if ( index < 3 ) {
+    return [0, 1, 2];
+  }
+  if ( index < 6 ) {
+    return [3, 4, 5];
+  }
+  return [6, 7, 8];
+}
+
+function getSquare( rows, ri, ci ) {
+  let rowRange = getRange( ri );
+  let colRange = getRange( ci );
+  const square = [];
+  for ( let rowIdx of rowRange ) {
+    for ( let colIdx of colRange ) {
+      square.push( rows[ rowIdx ][ colIdx ] );
+    }
+  }
+  return square;
+}
+
 function App() {
+  const n = null;
+  const [rows, setRows] = useState([
+    [1, n, n, n, n, n, n, n, n ],
+    [n, n, n, 1, n, n, 8, n, 2 ],
+    [n, n, 8, 9, n, n, 6, n, n ],
+    [4, n, n, 7, 6, n, n, 3, n ],
+    [n, 7, n, n, n, 4, n, 2, n ],
+    [n, 6, n, n, 1, 8, n, n, 7 ],
+    [n, n, 1, n, n, 9, 2, n, n ],
+    [2, n, 5, 6, n, 7, n, n, n ],
+    [n, n, n, n, n, n, n, n, 5 ],
+  ]);
+
+  /**
+   * Build list of possible values for a cell
+   *
+   * @param {Number|null} cell A known cell, or null
+   * @param {Number}      ri   The index of the row
+   * @param {Number}      ci   The index of the column
+   */
+  function computeGuess( cell, ri, ci ) {
+    const available = {
+      ...options,
+    };
+    for ( let num of rows[ ri ] ) {
+      if ( num ) {
+        available[ num ] = false;
+      }
+    }
+    for ( let row of rows ) {
+      if ( row[ ci ] ) {
+        available[ row[ ci ] ] = false;
+      }
+    }
+    for ( let num of getSquare( rows, ri, ci ) ) {
+      if ( num ) {
+        available[ num ] = false;
+      }
+    }
+    return Object.keys( available ).reduce( ( guesses, num ) => {
+      if ( available[num] ) {
+        return guesses.concat( num );
+      }
+      return guesses;
+    }, [] ).join( ', ' );
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table cellSpacing={0}>
+          {rows.map(( row, ri ) => (
+            <tr>{row.map(( cell, ci ) => (
+              <td>{ cell ? (
+                <span>{ cell }</span>
+              ) : (
+                <span className="guess">{ computeGuess( cell, ri, ci ) }</span>
+              )}</td>
+            ))}</tr>
+          ))}
+        </table>
       </header>
     </div>
   );
