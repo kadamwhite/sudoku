dea5edc5f766c968d85ef5b6e22197df1444358b
K. Adam White <kadamwhite@users.noreply.github.com>
2020-05-22 10:43:51 -0400

    Updates

---
+++ precache-manifest.36d3c7c9af5b0b871263925621697e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cd829abab9f263c4b7e7083bd60035",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "857ef613dbce9818ed2d",
+    "url": "/sudoku/static/css/main.1c031474.chunk.css"
+  },
+  {
+    "revision": "2193fe0f5011415a3de7",
+    "url": "/sudoku/static/js/2.b39a3d03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sudoku/static/js/2.b39a3d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857ef613dbce9818ed2d",
+    "url": "/sudoku/static/js/main.09ef9026.chunk.js"
+  },
+  {
+    "revision": "5e467a82d5680057536a",
+    "url": "/sudoku/static/js/runtime-main.e006c59b.js"
+  }
+]);
